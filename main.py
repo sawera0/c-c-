0d75be26b9a64e7667c184083627edf9ff097695
Sawera  jamshaid <sawerajamshaid0@gmail.com>
2024-12-16 09:47:24 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+
+#include <iostream> // Include the header for input/output
+using namespace std; // Use the standard namespace
+
+int main() {
+    int n; 
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    if (n % 2 == 0) { 
+        cout << "even" << endl;
+    } else {
+        cout << "odd" << endl;
+    }
+
+    return 0; 
